39f9a0b37e2045bde3a40fe74537faae431aad6d
m-hari16 <active.hari16@gmail.com>
2023-12-28 00:22:56 +0700

    prepare routing web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login-petugas', 'Web\AuthController@index');
+Route::post('login', 'Web\AuthController@login');
+
+Route::prefix('admin')->middleware('auth')->group(function() {
+    Route::resource('/perpustakaan', 'Web\PerpustakaanController');
+    Route::resource('/kategori-buku', 'Web\KategoriBukuController');
+    Route::resource('/buku', 'Web\BukuController');
+    Route::resource('/petugas', 'Web\PetugasController');
+    Route::resource('/anggota', 'Web\AnggotaController');
+    Route::resource('/kunjungan', 'Web\KunjunganController');
+    Route::resource('/peminjaman', 'Web\PeminjamanController');
+    Route::resource('/pengembalian', 'Web\PengembalianController');
+    Route::post('/logout', 'Web\AuthController@logout');
+});
